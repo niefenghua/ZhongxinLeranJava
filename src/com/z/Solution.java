4380e0eac6ac13e420a68939eaa79b887a59dc17
niefenghua <niefenghua@msn.cn>
2016-07-12 20:37:22 +0800

    ok

--- src/com/z/Solution.java
+++ src/com/z/Solution.java
@@ -12,12 +12,13 @@ public class Solution {
 	private Solution() {
 	}
 	
-	// 饿汉 一开始就加载
-	// private static Solution solution =new Solution();
-	// public static Solution getInstance() {
-	// // write your code here
-	// return solution;
-	// }
+	/* 饿汉 一开始就加载
+	 private static Solution solution =new Solution();
+	 public static Solution getInstance() {
+	 	return solution;
+	 }
+	 
+	 */
 
 	// 懒汉 使用时才加载
 	private static Solution solution = null;
